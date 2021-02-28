284b06f92893b295a186861db1b3b1e907c189e8
Pessoa <ppessoa@albany.edu>
2021-02-28 18:53:10 -0500

    curvature and metric as functions in IGQG

--- IGQG.py
+++ IGQG.py
@@ -1,4 +1,5 @@
 from mpmath import mp,polylog,re,gamma,det,power
+from numpy import zeros
 mp.dps = 35; mp.pretty = True
 
 def fpolylog(phi,x):
@@ -30,3 +31,18 @@ def Bc(phi,xi):
             [fgamma(phi+3)*fpolylog(phi+1,xi),fgamma(phi+2)*fpolylog(phi,xi),xi*(xi+1)*power((1-xi),-3)]]
     #print(matB)
     return det(matB)
+
+def bose_Determinant(a,ac,beta,eta): #Calculates the *unitless* metric g_f for bosons (41)
+    return a + ac*(beta**(eta+1))#ac*power(beta,(eta+1))
+
+def fermi_Determinant(a): #somewhat pointless function, calculates *unitless* metric g_f for fermions (32)
+    return a
+
+def bose_Curvature(a,ac,b,bc,beta,eta): #Calculates the *unitless* curvature R_f for bosons (44)
+    be= beta**(eta+1)#power(beta,(1+eta))
+    num =  (b+be*bc)
+    den = bose_Determinant(a,ac,beta,eta)**2
+    return -num/den
+
+def fermi_Curvature(a,b): #Calculates the *unitless* curvature R_f for fermions (32)
+    return b/(a**2)
