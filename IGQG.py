b8ef75bd5d48d2da56d2a56b58af78516bdfbd13
Pessoa <ppessoa@albany.edu>
2021-08-05 20:23:14 -0400

    prepare for v2

--- IGQG.py
+++ IGQG.py
@@ -1,13 +1,19 @@
-from mpmath import mp,polylog,re,gamma,det,power
-from numpy import zeros
+from mpmath import mp,polylog,re,gamma,det,power,zeta
+from numpy import zeros,array,ndarray
 mp.dps = 35; mp.pretty = True
 
+#Adaptation of mp functions
 def fpolylog(phi,x):
+    if isinstance(x, ndarray):
+        return array([fpolylog(phi,xx) for xx in x])
     return re(polylog(phi,x))
+
 def fgamma(phi):
     return re(gamma(phi))
 
+#matrix functions
 def A(phi,xi):
+    #print('here')
     matA = [[fgamma(phi+3)*fpolylog(phi+2,xi),fgamma(phi+2)*fpolylog(phi+1,xi)],
             [fgamma(phi+2)*fpolylog(phi+1,xi),fgamma(phi+1)*fpolylog(phi,xi)]]
     return det(matA)
@@ -32,6 +38,7 @@ def Bc(phi,xi):
     #print(matB)
     return det(matB)
 
+#Curvatures
 def bose_Determinant(a,ac,beta,eta): #Calculates the *unitless* metric g_f for bosons (41)
     return a + ac*(beta**(eta+1))#ac*power(beta,(eta+1))
 
@@ -46,3 +53,62 @@ def bose_Curvature(a,ac,b,bc,beta,eta): #Calculates the *unitless* curvature R_f
 
 def fermi_Curvature(a,b): #Calculates the *unitless* curvature R_f for fermions (32)
     return b/(a**2)
+
+#xi inversion functions
+def number(x,beta,eta,branch='be'):
+    if branch == 'fd':
+        return -gamma(eta+1)*power(beta,-eta-1)*fpolylog(eta+1,-x)
+    if branch == 'beexcited':
+        return gamma(eta+1)*power(beta,-eta-1)*fpolylog(eta+1,x)
+    Ne = gamma(eta+1)*power(beta,-eta-1)*fpolylog(eta+1,x)
+    N0 = x/(1-x)
+    return N0+Ne
+
+def xi(beta,eta,N,branch='be',est=1,epsilon=.1):
+    if isinstance(beta, ndarray):
+        xiarray = [xi(beta[0],eta,N,branch,est,epsilon)]
+        for i in range(1,len(beta)):
+            xiarray.append(xi(beta[i],eta,N,branch,xiarray[-1],epsilon))
+        return array(xiarray)
+    
+    i = 0
+    if branch == 'be':
+        mino,majo = 0,1
+        if est==1:
+            est = 1-1/N
+    elif branch =='fd':
+        mino,majo = 0,mp.floor(est+1)
+        while(N>number(majo,beta,eta,branch)):
+            i+=1
+            mino = majo
+            majo*=2
+            est = (majo+mino)/2
+
+    while(abs(N-number(est,beta,eta,branch))>epsilon):
+        i+=1
+        if(N>number(est,beta,eta,branch)):
+            mino = est
+        else:
+            majo = est
+        est = (majo+mino)/2
+    #print('Inversion for N={} and beta= {} -- attempts:{}'.format(int(N),beta,i))
+    return est
+
+def xiTL(betaratio,eta,branch='be',epsilon=1e-6):
+    if isinstance(betaratio, ndarray):
+        return array([xiTL(br,eta,branch,epsilon) for br in betaratio])
+    if betaratio>=1:
+        return 1
+    mino,majo = 0,1
+    i=0
+    est = mp.mpf(betaratio)
+    while (abs(betaratio**(eta+1)-fpolylog(eta+1,est)/zeta(eta+1))>epsilon):
+        if(betaratio**(eta+1)>fpolylog(eta+1,est)/zeta(eta+1)):
+            mino = est
+        else:
+            majo = est
+        est = (majo+mino)/2
+        i+=1
+    #print('Inversion for TL and beta= {} -- attempts:{}'.format(betaratio,i))
+    return est
+
